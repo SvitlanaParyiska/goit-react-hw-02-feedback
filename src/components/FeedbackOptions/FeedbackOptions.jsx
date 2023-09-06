0482ddac30619672b7d288122b0d2c296d5ea2e5
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-09-06 22:27:59 +0300

    commit

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button
+          key={option}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default FeedbackOptions;
